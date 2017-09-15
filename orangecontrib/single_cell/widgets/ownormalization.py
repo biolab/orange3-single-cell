e2dac672b1552973072b33176f35038ccaf95af0
mstrazar <martin.strazar@gmail.com>
2017-09-15 11:40:48 +0200

    Minor redesign and do not crash on no discrete variables.

--- orangecontrib/single_cell/widgets/ownormalization.py
+++ orangecontrib/single_cell/widgets/ownormalization.py
@@ -72,33 +72,33 @@ class OWNormalization(widget.OWWidget):
     log_check = settings.Setting(True)
     log_base = 2
 
-    class Warning(widget.OWWidget.Warning):
-        no_discrete_attributes = widget.Msg("No discrete variables in input data.")
-
     def __init__(self):
         self.data = None
         self.info = gui.label(self.controlArea, self,
                               "No data on input", box="Info")
 
         # Library / group variable
+        box0 = gui.vBox(
+            self.controlArea, "Data from multiple libraries")
+        self.equalize_check = gui.checkBox(box0,
+                     self, "equalize_lib",
+                     "Equalize library read depth on:",
+                     callback=self.on_changed_equalize,
+                     addSpace=True)
         attrs_model = self.attrs_model = DomainModel(
             order=(DomainModel.CLASSES, DomainModel.METAS),
             valid_types=DiscreteVariable)
         combo_attrs = self.combo_attrs = gui.comboBox(
-            self.controlArea, self, 'selected_attr',
-            box="Equalize read depth on column",
+            box0, self, 'selected_attr',
             callback=self.on_changed,
             sendSelectedValue=True)
         combo_attrs.setModel(attrs_model)
 
-
         # Steps and parameters
-        box = gui.widgetBox(self.controlArea, 'Steps and parameters')
-        gui.checkBox(gui.vBox(box), self, "equalize_lib", "Equalize library read depth",
+        box1 = gui.widgetBox(self.controlArea, 'Steps and parameters')
+        gui.checkBox(gui.vBox(box1), self, "normalize_cells", "Normalize cell profiles",
                      callback=self.on_changed)
-        gui.checkBox(gui.vBox(box), self, "normalize_cells", "Normalize cell profiles",
-                     callback=self.on_changed)
-        gui.spin(box, self, "log_base", 2.0, 10.0, label="Log(1 + x) transform, base: ",
+        gui.spin(box1, self, "log_base", 2.0, 10.0, label="Log(1 + x) transform, base: ",
                  checked="log_check", alignment=Qt.AlignRight,
                  callback=self.on_changed,
                  checkCallback=self.on_changed, controlWidth=60)
@@ -108,7 +108,6 @@ class OWNormalization(widget.OWWidget):
     def set_data(self, data):
         self.closeContext()
         self.data = data
-        self.Warning.clear()
 
         if self.data is None:
             self.attrs_model.clear()
@@ -120,27 +119,32 @@ class OWNormalization(widget.OWWidget):
                           (len(data), len(data.domain.attributes)))
 
         self.attrs_model.set_domain(data.domain)
+        self.equalize_check.setEnabled(len(self.attrs_model) > 0)
+        self.combo_attrs.setEnabled(self.equalize_lib)
         if len(self.attrs_model) != 0:
             self.selected_attr = str(self.attrs_model[0])
-        else:
-            self.Warning.no_discrete_attributes()
-            self.send("Data", None)
-            return
 
+        self.send("Data", None)
         self.openContext(self.data.domain)
         self.on_changed()
 
     def on_changed(self):
         self.commit()
 
+    def on_changed_equalize(self):
+        self.combo_attrs.setEnabled(self.equalize_lib)
+        self.commit()
+
     def commit(self):
-        if self.data is None or len(self.attrs_model) == 0 or \
-                        self.selected_attr not in self.data.domain:
+        if self.data is None:
             self.send("Data", None)
             return
 
-        library_var = self.data.domain[self.selected_attr]
-        library_values, _ = self.data.get_column_view(library_var)
+        if self.equalize_lib and self.selected_attr in self.data.domain:
+            library_var = self.data.domain[self.selected_attr]
+            library_values, _ = self.data.get_column_view(library_var)
+        else:
+            library_values = None
         log_base = self.log_base if self.log_check else None
         X_normed = normalize(self.data.X,
                              y=library_values,
