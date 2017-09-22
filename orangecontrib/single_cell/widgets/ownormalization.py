a37cd16097c962fe1619f633fbb277502bb31afa
mstrazar <martin.strazar@gmail.com>
2017-09-22 15:25:08 +0200

    Modify widget to correctly return the preprocessor.

--- orangecontrib/single_cell/widgets/ownormalization.py
+++ orangecontrib/single_cell/widgets/ownormalization.py
@@ -10,6 +10,8 @@ class OWNormalization(widget.OWWidget):
     icon = 'icons/Normalization.svg'
     priority = 110
 
+    DEFAULT_CELL_NORM = "(One group per cell)"
+
     inputs = [("Data", Table, 'set_data')]
     outputs = [("Data", Table), ("Preprocessor", ScNormalizeProjection)]
 
@@ -17,10 +19,9 @@ class OWNormalization(widget.OWWidget):
     resizing_enabled = False
 
     settingsHandler = settings.DomainContextHandler(metas_in_res=True)
-    selected_attr = settings.ContextSetting("")
+    selected_attr = settings.Setting(DEFAULT_CELL_NORM)
     autocommit = settings.Setting(True)
 
-    equalize_lib = settings.Setting(True)
     normalize_cells = settings.Setting(True)
     log_check = settings.Setting(True)
     log_base = 2
@@ -33,12 +34,13 @@ class OWNormalization(widget.OWWidget):
         # Library / group variable
         box0 = gui.vBox(
             self.controlArea, "Data from multiple libraries")
-        self.equalize_check = gui.checkBox(box0,
-                     self, "equalize_lib",
-                     "Equalize library read depth on:",
-                     callback=self.on_changed_equalize,
-                     addSpace=True)
+        self.normalize_check = gui.checkBox(box0,
+                                self, "normalize_cells",
+                                "Normalize cell profiles on:",
+                                callback=self.on_changed_normalize,
+                                addSpace=True)
         attrs_model = self.attrs_model = DomainModel(
+            placeholder=self.DEFAULT_CELL_NORM,
             order=(DomainModel.CLASSES, DomainModel.METAS),
             valid_types=DiscreteVariable)
         combo_attrs = self.combo_attrs = gui.comboBox(
@@ -48,9 +50,7 @@ class OWNormalization(widget.OWWidget):
         combo_attrs.setModel(attrs_model)
 
         # Steps and parameters
-        box1 = gui.widgetBox(self.controlArea, 'Steps and parameters')
-        gui.checkBox(gui.vBox(box1), self, "normalize_cells", "Normalize cell profiles",
-                     callback=self.on_changed)
+        box1 = gui.widgetBox(self.controlArea, 'Further steps and parameters')
         gui.spin(box1, self, "log_base", 2.0, 10.0, label="Log(1 + x) transform, base: ",
                  checked="log_check", alignment=Qt.AlignRight,
                  callback=self.on_changed,
@@ -72,10 +72,8 @@ class OWNormalization(widget.OWWidget):
                           (len(data), len(data.domain.attributes)))
 
         self.attrs_model.set_domain(data.domain)
-        self.equalize_check.setEnabled(len(self.attrs_model) > 0)
-        self.combo_attrs.setEnabled(self.equalize_lib)
-        if len(self.attrs_model) != 0:
-            self.selected_attr = str(self.attrs_model[0])
+        self.normalize_check.setEnabled(len(self.attrs_model) > 0)
+        self.combo_attrs.setEnabled(self.normalize_cells)
 
         self.send("Data", None)
         self.openContext(self.data.domain)
@@ -84,15 +82,15 @@ class OWNormalization(widget.OWWidget):
     def on_changed(self):
         self.commit()
 
-    def on_changed_equalize(self):
-        self.combo_attrs.setEnabled(self.equalize_lib)
+    def on_changed_normalize(self):
+        self.combo_attrs.setEnabled(self.normalize_cells)
         self.commit()
 
     def commit(self):
         log_base = self.log_base if self.log_check else None
         library_var, Y = None, None
         if self.data is not None and \
-                self.equalize_lib and \
+                self.normalize_cells and \
                 self.selected_attr in self.data.domain:
             library_var = self.data.domain[self.selected_attr]
             Y, _ = self.data.get_column_view(library_var)
@@ -100,19 +98,11 @@ class OWNormalization(widget.OWWidget):
 
         # Faster execution if model is fit in-place.
         model = ScNormalizeModel(equalize_var=library_var,
-                                        normalize_cells=self.normalize_cells,
-                                        log_base=log_base)
+                                 normalize_cells=self.normalize_cells,
+                                 log_base=log_base)
         model.fit(X=self.data.X, Y=Y)
         new_data = model.transform(self.data) if self.data is not None else None
         projection = ScNormalizeProjection(model, self.data.domain)
-
-        # Slower execution for projection
-        # projector = ScNormalizeProjector(self.data.domain,
-        #                                 equalize_var=library_var,
-        #                                 normalize_cells=self.normalize_cells,
-        #                                 log_base=log_base)
-        # projection = projector.fit(X=self.data.X, Y=Y)
-        # new_data = projection(self.data) if self.data is not None else None
 
         self.send("Data", new_data)
         self.send("Preprocessor", projection)
