202e22abb55389cb72ed9f93145932176b358e96
mstrazar <martin.strazar@gmail.com>
2017-10-10 16:59:03 +0200

    Simplify code and assure correct return values.

--- orangecontrib/single_cell/preprocess/scnorm.py
+++ orangecontrib/single_cell/preprocess/scnorm.py
@@ -11,7 +11,7 @@ class QuantRegLearner(Learner):
 
     Parameters
     ----------
-    degree : float in [0, 1]
+    quantile : float in [0, 1]
         target quantile
     preprocessors : List[Preprocessor]
         preprocessors to be applied on the data before learning
@@ -90,62 +90,83 @@ def scnorm(data, subgroup_frac = 0.25, K = 10):
 
     # Process each group independently
     for k in range(K):
+
         print("Processing group %d ..." % k)
-        group_slopes = slopes[groups == k]
+        group_inxs = np.where(groups == k)[0]
+        group_slopes = slopes[group_inxs]
         group_median = np.median(group_slopes)
-        group_inxs = np.argsort(np.absolute(group_slopes - group_median))[:max(2, int(subgroup_frac * len(group_slopes)))]
-        group_vars = [valid_vars[i] for i in np.where(groups == k)[0][group_inxs]]
+        group_vars = [valid_vars[i] for i in group_inxs]
         group_counts = data[:, group_vars].X
 
-        # Fill submodel data
+        # Select subgroup_frac (default 25%) genes for faster fitting
+        n_subgroup = max(2, int(subgroup_frac * len(group_slopes)))
+        subgroup_inxs = group_inxs[np.argsort(np.absolute(group_slopes - group_median))[:n_subgroup]]
+        subgroup_vars = [valid_vars[i] for i in subgroup_inxs]
+        subgroup_counts = data[:, subgroup_vars].X
+
+        # Convert to long format (whole group)
         nz = np.nonzero(group_counts)
-        group_D = np.zeros((len(nz[0]), 1))
-        group_Y = np.zeros((len(nz[0]), 1))
+        D_all = np.zeros((len(nz[0]), 1))
+        Y_all = np.zeros((len(nz[0]), 1))
         for i, (r, c) in enumerate(zip(*nz)):
-            group_D[i] = D[r]
-            group_Y[i] = group_counts[r, c]
+            D_all[i] = D[r]
+            Y_all[i] = group_counts[r, c]
+
+        # Convert to long format (sub-group)
+        nz_sub = np.nonzero(subgroup_counts)
+        D_sub = np.zeros((len(nz_sub[0]), 1))
+        Y_sub = np.zeros((len(nz_sub[0]), 1))
+        for i, (r, c) in enumerate(zip(*nz_sub)):
+            D_sub[i] = D[r]
+            Y_sub[i] = subgroup_counts[r, c]
 
         # Fit submodels for different quantiles and select best model
         submodels = dict()
-        q_best = degree_best = q_distance = float("inf")
         for q, degree in it.product(q_range, degree_range):
-            Dp = PolynomialFeatures(degree=degree).fit_transform(np.log(group_D))
-            submodels[q, degree] = QuantRegLearner(quantile=q).fit(Dp,
-                                                                   np.log(group_Y),
-                                                                   W=None)
-            curr_dist = np.absolute(group_median - submodels[q, degree].params[1])
-            if curr_dist < q_distance:
-                q_distance = curr_dist
-                q_best = q
-                degree_best = degree
+            Dp = PolynomialFeatures(degree=degree).fit_transform(np.log(D_sub))
+            model = QuantRegLearner(quantile=q).fit(Dp, np.log(Y_sub), W=None)
+            model_dist = np.absolute(group_median - model.params[1])
+            submodels[q, degree] =  model_dist, model
+
+        # Select best model with closest distance to slope
+        (q_best, degree_best), (_, model_best) = sorted(submodels.items(), key=lambda tup: tup[1])[0]
         print("Best model parameters (q=%.2f, d=%d)" % (q_best, degree_best))
 
         # Select best model and use normalized values
-        Dp = PolynomialFeatures(degree=degree_best).fit_transform(np.log(group_D))
-        log_predicted_expression = submodels[q_best, degree_best].predict(Dp)
-        gene_expression = sp.stats.scoreatpercentile(np.log(group_Y), 100 * q_best)
+        Dp = PolynomialFeatures(degree=degree_best).fit_transform(np.log(D_all))
+        log_predicted_expression = model_best.predict(Dp)
+        gene_expression = sp.stats.scoreatpercentile(np.log(Y_all), 100 * q_best)
         size_factors = np.exp(log_predicted_expression) / np.exp(gene_expression)
-        new_data[:, group_vars].X[nz] = new_data[:, group_vars].X[nz] / size_factors
+
+        # Fill the values in the original domain
+        orig_cols = list(map(lambda v: data.domain._indices[v], group_vars))
+        new_X = new_data.X[:, orig_cols].copy()
+        new_X[nz] = new_X[nz] / size_factors
+        new_data.X[:, orig_cols] = new_X
 
     # Return data with a normalized matrix
     return new_data
 
 
-
-if __name__ == "__main__":
+def test():
     from Orange.data.table import Table
     import matplotlib.pyplot as plt
     test_file = "/Users/martin/Dev/data/singlecell/zhang2017/tables/matrix_counts_sample.tab"
     data = Table(test_file)
-    cols = np.where((data.X > 0).sum(axis=0) > 1)[0]
-    data = data[:, cols]
     data.X *= np.random.rand(*data.X.shape)
 
+
     # Normalize original data
-    new_data = scnorm(data, K=1)
+    normed_data = scnorm(data, K=10)
+    print(np.linalg.norm(normed_data.X - data.X))
+
     x = np.log(data.X.ravel() + 1)
-    y = np.log(new_data.X.ravel() + 1)
+    y = np.log(normed_data.X.ravel() + 1)
 
     plt.figure()
     plt.plot(x, y, ".")
     plt.show()
+
+
+if __name__ == "__main__":
+    test()
