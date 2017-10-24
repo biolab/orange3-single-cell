b96e1dde076b88e86a631338009b260037937e7b
mstrazar <martin.strazar@gmail.com>
2017-10-24 20:13:23 +0200

    Implement scNorm and parameters.

--- orangecontrib/single_cell/widgets/ownormalization.py
+++ orangecontrib/single_cell/widgets/ownormalization.py
@@ -3,31 +3,44 @@ from AnyQt.QtCore import Qt, QTimer
 from Orange.data import Table, DiscreteVariable
 from Orange.widgets import widget, gui, settings
 from Orange.widgets.utils.itemmodels import DomainModel
+from Orange.preprocess.preprocess import Preprocess
 
 from orangecontrib.single_cell.preprocess.scnormalize import SCNormalizer
+from orangecontrib.single_cell.preprocess.scnorm import ScNormPreprocessor
 
 
 class OWNormalization(widget.OWWidget):
     name = 'Single Cell Normalization'
-    description = ('Basic normalization of single cell count data')
+    description = ('Normalization of single cell count data')
     icon = 'icons/Normalization.svg'
     priority = 110
 
-    DEFAULT_CELL_NORM = "(One group per cell)"
+
+    # Default grouping model
+    GROUPING_CELL_GROUP = "None"
+
+    # Normalization models
+    MODEL_CELL_MEDIAN = 0
+    MODEL_SCNORM = 1
 
     inputs = [("Data", Table, 'set_data')]
-    outputs = [("Data", Table), ("Preprocessor", SCNormalizer)]
+    outputs = [("Data", Table), ("Preprocessor", Preprocess)]
 
     want_main_area = False
     resizing_enabled = False
 
     settingsHandler = settings.DomainContextHandler(metas_in_res=True)
-    selected_attr = settings.Setting(DEFAULT_CELL_NORM)
+    selected_attr = settings.Setting(GROUPING_CELL_GROUP)
     autocommit = settings.Setting(True)
 
+    model_select = settings.Setting(MODEL_CELL_MEDIAN)
     normalize_cells = settings.Setting(True)
     log_check = settings.Setting(True)
-    log_base = 2
+    log_base = settings.Setting(2)
+
+    # ScNorm-specific settings
+    scnorm_num_groups = settings.Setting(10)
+    scnorm_p_subgroup = settings.Setting(5)
 
     def __init__(self):
         self.data = None
@@ -35,22 +48,34 @@ class OWNormalization(widget.OWWidget):
                               "No data on input", box="Info")
 
         # Library / group variable
-        box0 = gui.vBox(
-            self.controlArea, "Data from multiple libraries")
-        self.normalize_check = gui.checkBox(box0,
-                                self, "normalize_cells",
-                                "Normalize cell profiles on:",
-                                callback=self.on_changed_normalize,
-                                addSpace=True)
+        box0 = gui.vBox(self.controlArea, "Normalization model")
+        box0.layout().setSpacing(7)
+        model_selection = gui.radioButtons(
+            widget=box0,
+            master=self,
+            value="model_select",
+            callback=self.on_changed,
+            btnLabels=("Median", "scNorm"))
+        model_selection.layout().setSpacing(7)
+
+        # Cell grouping
         attrs_model = self.attrs_model = DomainModel(
-            placeholder=self.DEFAULT_CELL_NORM,
+            placeholder=self.GROUPING_CELL_GROUP,
             order=(DomainModel.CLASSES, DomainModel.METAS),
             valid_types=DiscreteVariable)
         combo_attrs = self.combo_attrs = gui.comboBox(
             box0, self, 'selected_attr',
+            label="Group by:",
             callback=self.on_changed,
             sendSelectedValue=True)
         combo_attrs.setModel(attrs_model)
+
+        # ScNorm specific
+        # Steps and parameters
+        self.scnorm_spin_n = gui.spin(box0, self, "scnorm_num_groups", 1, 20, step=1, label="Num. of gene groups: ",
+                 alignment=Qt.AlignRight, callback=self.on_changed, checkCallback=self.on_changed, controlWidth=60)
+        self.scnorm_spin_p = gui.spin(box0, self, "scnorm_p_subgroup", 0, 100, step=1, label="Prototype genes (%): ",
+                 alignment=Qt.AlignRight, callback=self.on_changed, checkCallback=self.on_changed, controlWidth=60)
 
         # Steps and parameters
         box1 = gui.widgetBox(self.controlArea, 'Further steps and parameters')
@@ -76,18 +101,14 @@ class OWNormalization(widget.OWWidget):
                           (len(data), len(data.domain.attributes)))
 
         self.attrs_model.set_domain(data.domain)
-        self.normalize_check.setEnabled(len(self.attrs_model) > 0)
-        self.combo_attrs.setEnabled(self.normalize_cells)
 
         self.send("Data", None)
         self.openContext(self.data.domain)
         self.on_changed()
 
     def on_changed(self):
-        self.commit()
-
-    def on_changed_normalize(self):
-        self.combo_attrs.setEnabled(self.normalize_cells)
+        self.scnorm_spin_n.setEnabled(self.model_select == self.MODEL_SCNORM)
+        self.scnorm_spin_p.setEnabled(self.model_select == self.MODEL_SCNORM)
         self.commit()
 
     def commit(self):
@@ -98,9 +119,16 @@ class OWNormalization(widget.OWWidget):
                 self.selected_attr in self.data.domain:
             library_var = self.data.domain[self.selected_attr]
 
-        pp = SCNormalizer(equalize_var=library_var,
-                          normalize_cells=self.normalize_cells,
-                          log_base=log_base)
+        if self.model_select == self.MODEL_CELL_MEDIAN:
+            pp = SCNormalizer(equalize_var=library_var,
+                              normalize_cells=self.normalize_cells,
+                              log_base=log_base)
+        elif self.model_select == self.MODEL_SCNORM:
+            pp = ScNormPreprocessor(equalize_var=library_var,
+                                    p_subgroup=self.scnorm_p_subgroup / 100.0,
+                                    K=self.scnorm_p_subgroup,
+                                    log_base=log_base)
+
         data = None
         if self.data is not None:
             data = pp(self.data)
@@ -118,7 +146,8 @@ if __name__ == "__main__":
 
     # Load test file from arguments
     test_file = argv[1] if len(argv) >= 2 else "matrix_counts_sample.tab"
-    table = Table(test_file)
+    test_file = "/Users/martin/Dev/data/singlecell/bacher2017/H1_data.csv"
+    table = Table(test_file)[:100, :100]
     ow.set_data(table)
 
     ow.show()
