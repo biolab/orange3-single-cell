917c117211ca254eaa60ba9fd1df7d71bb3728df
astaric <anze.staric@gmail.com>
2017-12-01 09:48:23 +0100

    welcome: reorder buttons

--- orangecontrib/single_cell/launcher/welcome.py
+++ orangecontrib/single_cell/launcher/welcome.py
@@ -788,7 +788,7 @@ def welcome_dialog_paged(self):
         "New", toolTip="Create a new workflow")
     s = QShortcut(QKeySequence.New, newbutton)
     s.activated.connect(newbutton.click)
-    buttons.addButton(newbutton, QDialogButtonBox.ActionRole)
+    buttons.addButton(newbutton, QDialogButtonBox.AcceptRole)
 
     openexisting = QPushButton(
         "Open Existing\N{HORIZONTAL ELLIPSIS}",
@@ -796,7 +796,7 @@ def welcome_dialog_paged(self):
     )
     s = QShortcut(QKeySequence.Open, dlg)
     s.activated.connect(openexisting.click)
-    buttons.addButton(openexisting, QDialogButtonBox.ActionRole)
+    buttons.addButton(openexisting, QDialogButtonBox.AcceptRole)
 
     settings = QSettings()
 
@@ -805,7 +805,9 @@ def welcome_dialog_paged(self):
         "Show at startup",
         checked=settings.value(show_start_key, True, type=bool)
     )
-    buttons.addButton(show_start, QDialogButtonBox.ActionRole)
+    # Abusing ResetRole to push the check box to the left in all button
+    # layouts.
+    buttons.addButton(show_start, QDialogButtonBox.ResetRole)
 
     def update_show_at_startup(value):
         settings.setValue(show_start_key, value)
