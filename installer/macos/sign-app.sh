fdf2eba61024f9d0ebed6b36c3e53d5559061269
astaric <anze.staric@gmail.com>
2017-12-04 09:04:20 +0100

    installer: cleanup sign-app script

--- installer/macos/sign-app.sh
+++ installer/macos/sign-app.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-# Build app
-rm -rf ~/dev/orange3-single-cell/dist/scOrange.app
-./build-macos-app.sh ~/dev/orange3-single-cell/dist/scOrange.app
-# Missing symlink Current messes with code signing
-ln -s 3.6 ../../dist/scOrange.app/Contents/Frameworks/Python.framework/Versions/Current
-# sign bundle
-codesign -s "Developer ID" /Users/anze/dev/orange3-single-cell/dist/scOrange.app/Contents/Frameworks/Python.framework/Versions/3.6
-codesign -s "Developer ID" /Users/anze/dev/orange3-single-cell/dist/scOrange.app/Contents/MacOS/pip
-codesign -s "Developer ID" /Users/anze/dev/orange3-single-cell/dist/scOrange.app
+DIST="$( cd "$(dirname "$0")/../../dist" ; pwd -P )"
+APP=$DIST/scOrange.app
 
+# Build app
+rm -rf $APP
+./build-macos-app.sh $APP
+# Missing symlink Current messes with code signing
+ln -s 3.6 $APP/Contents/Frameworks/Python.framework/Versions/Current
+# sign bundle
+codesign -s "Developer ID" $APP/Contents/Frameworks/Python.framework/Versions/3.6
+codesign -s "Developer ID" $APP/Contents/MacOS/pip
+codesign -s "Developer ID" $APP
+
+VERSION=$APP/Contents/MacOS/python -c 'import pkg_resources; print(pkg_resources.get_distribution("Orange3-SingleCell").version)'
 # Create disk image
-./create-dmg-installer.sh --app ../../dist/scOrange.app ../../dist/scOrange-1.0.0-alpha.dmg
+./create-dmg-installer.sh --app $APP $DIST/scOrange-$VERSION.dmg
 # Sign disk image
-codesign -s "Developer ID" /Users/anze/dev/orange3-single-cell/dist/scOrange-1.0.0-alpha.dmg
+codesign -s "Developer ID" $DIST/scOrange-$VERSION.dmg
