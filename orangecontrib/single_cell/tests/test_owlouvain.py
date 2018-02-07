8ff50b422368f286912a542eced15736d29ccc8b
Pavlin Poličar <pavlin.g.p@gmail.com>
2018-02-07 09:37:11 +0100

    OWLouvain: Sort cluster names by number of instances

---
+++ orangecontrib/single_cell/tests/test_owlouvain.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+from Orange.data import Table, Domain
+from Orange.widgets.tests.base import WidgetTest
+from orangecontrib.single_cell.widgets.owlouvainclustering import \
+    OWLouvainClustering
+
+
+# Deterministic tests
+np.random.seed(42)
+
+
+class TestOWLouvain(WidgetTest):
+    def setUp(self):
+        self.widget = self.create_widget(
+            OWLouvainClustering, stored_settings={'auto_commit': False}
+        )
+
+    def tearDown(self):
+        self.widget.onDeleteWidget()
+        super().tearDown()
+
+    def test_clusters_ordered_by_size(self):
+        """Cluster names should be sorted based on the number of instances."""
+        x1 = np.array([[0, 0]] * 20)
+        x2 = np.array([[1, 0]] * 15)
+        x3 = np.array([[0, 1]] * 10)
+        x4 = np.array([[1, 1]] * 5)
+        data = np.vstack((x1, x2, x3, x4))
+        # Remove any order depencence in data, not that this should affect it
+        np.random.shuffle(data)
+
+        table = Table.from_numpy(domain=Domain.from_numpy(X=data), X=data)
+        self.send_signal(self.widget.Inputs.data, table)
+        self.widget.k_neighbours = 4
+        self.widget.commit(force=True)
+        output = self.get_output(self.widget.Outputs.annotated_data, wait=1000)
+
+        clustering = output.get_column_view('Cluster')[0].astype(int)
+        counts = np.bincount(clustering)
+        np.testing.assert_equal(counts, sorted(counts, reverse=True))
