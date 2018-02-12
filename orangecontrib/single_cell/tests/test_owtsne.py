732ebedb0cd4cf52e4cb10c4af519c0c4a0199f1
Lan Zagar <lan.zagar@fri.uni-lj.si>
2018-02-12 12:34:41 +0100

    test_owtsne: Test error handling

--- orangecontrib/single_cell/tests/test_owtsne.py
+++ orangecontrib/single_cell/tests/test_owtsne.py
@@ -13,6 +13,7 @@ class TestOWtSNE(WidgetTest):
         self.class_var = DiscreteVariable('Stage name', values=['STG1', 'STG2'])
         self.attributes = [ContinuousVariable('GeneName' + str(i)) for i in range(5)]
         self.domain = Domain(self.attributes, class_vars=self.class_var)
+        self.empty_domain = Domain([], class_vars=self.class_var)
 
         self.data = None
 
@@ -21,10 +22,33 @@ class TestOWtSNE(WidgetTest):
         self.send_signal(self.widget.Inputs.data, self.data)
         self.assertIsNone(self.widget.data)
 
-        # no rows
+        # <2 rows
         self.data = Table(self.domain, [[1, 2, 3, 4, 5, 'STG1']])
         self.send_signal(self.widget.Inputs.data, self.data)
         self.assertIsNone(self.widget.data)
+        self.assertTrue(self.widget.Error.not_enough_rows.is_shown())
+
+        # no attributes
+        self.data = Table(self.empty_domain, [['STG1']] * 2)
+        self.send_signal(self.widget.Inputs.data, self.data)
+        self.assertIsNone(self.widget.data)
+        self.assertTrue(self.widget.Error.no_attributes.is_shown())
+
+        # constant data
+        self.data = Table(self.domain, [[1, 2, 3, 4, 5, 'STG1']] * 2)
+        self.send_signal(self.widget.Inputs.data, self.data)
+        self.assertIsNone(self.widget.data)
+        self.assertTrue(self.widget.Error.constant_data.is_shown())
+
+        # correct input
+        self.data = Table(self.domain, [[1, 2, 3, 4, 5, 'STG1'],
+                                        [5, 4, 3, 2, 1, 'STG1']])
+        self.send_signal(self.widget.Inputs.data, self.data)
+        self.assertIsNotNone(self.widget.data)
+        self.assertFalse(self.widget.Error.not_enough_rows.is_shown())
+        self.assertFalse(self.widget.Error.no_attributes.is_shown())
+        self.assertFalse(self.widget.Error.constant_data.is_shown())
+
 
     def test_input(self):
         self.data = Table(self.domain, [[1, 1, 1, 1, 1, 'STG1'],
