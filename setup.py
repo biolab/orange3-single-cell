aee1394ff378b1bc5bb68a30fdc914943bc12a24
astaric <anze.staric@gmail.com>
2018-02-22 15:55:10 +0100

    Release 0.3.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import subprocess
 
 NAME = "Orange3-SingleCell"
 
-VERSION = "0.3.1"
+VERSION = "0.3.2"
 
 DESCRIPTION = "Add-on for bioinformatics analysis of single cell data"
 LONG_DESCRIPTION = open(path.join(path.dirname(__file__), 'README.md')).read()
