ccd4f240004b4c3795015a6f4e2b8aa9d543dc66
JakaKokosar <jaka.kokosar@gmail.com>
2018-04-18 18:57:19 +0200

    owscorecells: match genes by gene_id attribute

--- orangecontrib/single_cell/widgets/owscorecells.py
+++ orangecontrib/single_cell/widgets/owscorecells.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from AnyQt.QtCore import QSize
+
 from Orange.data import ContinuousVariable, StringVariable, Domain, Table
 from Orange.widgets import widget, gui
 from Orange.widgets.settings import (Setting, ContextSetting,
@@ -8,6 +10,8 @@ from Orange.widgets.utils.itemmodels import DomainModel
 from Orange.widgets.utils.signals import Output, Input
 from Orange.widgets.utils.sql import check_sql_input
 from Orange.widgets.widget import OWWidget, Msg
+
+from orangecontrib.bioinformatics.ncbi.gene.config import MAP_GENE_ID
 
 
 class OWScoreCells(widget.OWWidget):
@@ -22,9 +26,18 @@ class OWScoreCells(widget.OWWidget):
 
     want_main_area = False
 
+    UserAdviceMessages = [
+        widget.Message("Gene variables must have 'gene_id' attribute! "
+                       "See example data in datasets widget.",
+                       'gene_id')]
+
+    class Error(OWWidget.Error):
+        no_gene_ids = Msg("gene_id not found")
+        column_name = Msg("Set column name")
+
     class Warning(OWWidget.Warning):
         no_genes = Msg("No matching genes in data")
-        some_genes = Msg("{} (of {}) genes not found in data")
+        imperfect_match = Msg("Some input genes can't be matched")
 
     class Inputs:
         data = Input("Data", Table)
@@ -37,20 +50,44 @@ class OWScoreCells(widget.OWWidget):
         super().__init__()
 
         self.data = None
+        self.data_genes = set()
+
         self.genes = None
+        self.marker_genes = set()
         self.feature_model = DomainModel(valid_types=StringVariable)
 
-        box = gui.vBox(self.controlArea, "Gene name")
+        self.score_variable_name = ''
+
+        box = gui.vBox(self.controlArea, "Gene name: ")
         gui.comboBox(box, self, 'gene', sendSelectedValue=True,
                      model=self.feature_model, callback=self._invalidate)
 
+        box = gui.vBox(self.controlArea, "Score output: ")
+        self.line_edit = gui.lineEdit(box, self, 'score_variable_name', callback=self._invalidate)
+        self.line_edit.setPlaceholderText('Column name ...')
+
         self.apply_button = gui.auto_commit(
             self.controlArea, self, "auto_apply", "&Apply", box=False)
+
+        self.sizeHint()
+
+    def sizeHint(self):
+        return QSize(320, 240)
+
+    def handleNewSignals(self):
+        self._invalidate()
 
     @Inputs.data
     @check_sql_input
     def set_data(self, data):
         self.data = data
+        if self.data:
+            for variable in self.data.domain.attributes:
+                self.data_genes.add(variable.attributes.get(MAP_GENE_ID, None))
+
+        self.data_genes = set(filter(None, self.data_genes))
+        if not self.data_genes:
+            self.Error.no_gene_ids()
 
     @Inputs.genes
     @check_sql_input
@@ -64,39 +101,42 @@ class OWScoreCells(widget.OWWidget):
             if self.feature_model:
                 self.gene = self.feature_model[0]
                 self.openContext(genes)
+                self.marker_genes = set(self.genes.get_column_view(self.gene)[0])
 
-    def handleNewSignals(self):
-        self._invalidate()
+    def __score_cells(self):
+        score = np.zeros(len(self.data))
+        matched = [gene_id for gene_id in self.data_genes.intersection(self.marker_genes)]
+
+        if not matched:
+            self.Warning.no_genes()
+        else:
+            values = self.data[:, matched].X
+            score = np.nanmax(values, axis=1)
+
+        return score
 
     def commit(self):
-        self.clear_messages()
-
         if self.data is None:
             self.Outputs.data.send(None)
             return
 
-        score = np.zeros(len(self.data))
-        if self.genes and self.gene:
-            available_genes = set(f.name for f in self.data.domain.variables)
-            gene_list_all = [str(ins[self.gene]) for ins in self.genes]
-            gene_list = [g for g in gene_list_all if g in available_genes]
-            if not gene_list:
-                self.Warning.no_genes()
-            else:
-                if len(gene_list) < len(gene_list_all):
-                    self.Warning.some_genes(len(gene_list_all) - len(gene_list),
-                                            len(gene_list_all))
-                values = self.data[:, gene_list].X
-                score = np.nanmax(values, axis=1)
+        if not self.score_variable_name:
+            self.Error.column_name()
+            return
 
-        d = self.data.domain
-        score_var = ContinuousVariable('Score')
-        dom = Domain(d.attributes, d.class_vars,
-                     d.metas + (score_var,))
-        table = self.data.transform(dom)
+        score_var = ContinuousVariable(self.score_variable_name)
+
+        score = self.__score_cells()
+        domain = Domain(self.data.domain.attributes,
+                        self.data.domain.class_vars,
+                        self.data.domain.metas + (score_var,))
+
+        table = self.data.transform(domain)
         col, sparse = table.get_column_view(score_var)
         col[:] = score
+
         self.Outputs.data.send(table)
+        self.clear_messages()
 
     def _invalidate(self):
         self.commit()
