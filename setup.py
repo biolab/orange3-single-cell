f654a9fd601426791abbd1474339583c4fe756a1
Lan Zagar <lan.zagar@fri.uni-lj.si>
2018-05-08 10:44:39 +0200

    Release 0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import subprocess
 
 NAME = "Orange3-SingleCell"
 
-VERSION = "0.4.0"
+VERSION = "0.5.0"
 
 DESCRIPTION = "Add-on for bioinformatics analysis of single cell data"
 LONG_DESCRIPTION = open(path.join(path.dirname(__file__), 'README.md')).read()
