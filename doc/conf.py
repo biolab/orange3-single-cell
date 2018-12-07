d663ef6c9d17d987ad427d8a4000b7e7d5c28567
Ajda Pretnar <ajdapretnar@gmail.com>
2018-12-07 11:20:12 +0100

    Setup index

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,7 @@ templates_path = ['templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = ['.rst']
+source_suffix = ['.md']
 #source_parsers = {".md": "recommonmark.parser.CommonMarkParser"}
 
 # The encoding of source files.
@@ -141,7 +141,7 @@ html_theme = 'nature'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+# html_static_path = ['static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
