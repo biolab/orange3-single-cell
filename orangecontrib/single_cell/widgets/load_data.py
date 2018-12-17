96e1d9e5a7db913cefbea0a51b934eea6bc813be
Vesna Tanko <tankovesna@hotmail.com>
2018-12-17 11:20:07 +0100

    ExcelLoader: Speedup setting file parameters

--- orangecontrib/single_cell/widgets/load_data.py
+++ orangecontrib/single_cell/widgets/load_data.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import scipy.io
 import loompy as lp
+import xlrd
 
 from Orange.data import (
     ContinuousVariable, DiscreteVariable, StringVariable, Domain, Table
@@ -587,6 +588,15 @@ class PickleLoader(Loader):
 
 
 class ExcelLoader(Loader):
+    def _set_file_parameters(self):
+        try:
+            sheet = xlrd.open_workbook(self._file_name).sheet_by_index(0)
+            self.n_cols = sheet.ncols
+            self.n_rows = sheet.nrows
+            self._set_sparsity()
+        except Exception:
+            pass
+
     @staticmethod
     def df_read_func(*args, **kwargs):
         return pd.read_excel(*args, **kwargs)
