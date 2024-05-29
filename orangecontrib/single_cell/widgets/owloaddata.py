58a7e3e2fd87113fe0e619a8aacca8bf7a718b0d
lenatr99 <lt89715@student.uni-lj.si>
2024-05-29 11:48:59 +0200

    Small fix

--- orangecontrib/single_cell/widgets/owloaddata.py
+++ orangecontrib/single_cell/widgets/owloaddata.py
@@ -613,7 +613,6 @@ class OWLoadData(widget.OWWidget):
             pathitem = RecentPath.create(filename, [])
             index = insert_recent_path(m, pathitem)
             self.row_annotations_combo.setCurrentIndex(index)
-            self._row_annotations_combo_changed()
             self._invalidate()
 
     @Slot()
@@ -633,7 +632,6 @@ class OWLoadData(widget.OWWidget):
             pathitem = RecentPath.create(filename, [])
             index = insert_recent_path(m, pathitem)
             self.col_annotations_combo.setCurrentIndex(index)
-            self._col_annotations_combo_changed()
             self._invalidate()
 
     def _invalidate(self):
